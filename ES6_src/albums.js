710c208b34c7b53e07ef958f14187765a5f4c551
kvivishvil1 <skvri15@freeuni.edu.ge>
2018-07-06 23:01:46 +0400

    Albums JS
    Albums JS

---
+++ ES6_src/albums.js
@@ -0,0 +1,19 @@
+function createAlbumItem() {
+    var html = "";
+    html+="<div class='album-list-item '> "
+    html+="     <img src='../other/covers/linkin_park.jpg' class='album-cover' alt='?'>"
+    html+="     <div class='album-name'>"
+    html+="         Album Name"
+    html+="     </div>"
+    html+="</div>"
+    return html;
+}
+
+function createAlbumList() {
+    var albumList = "";
+    albumList += "<div class='album-list clearfix'>";
+    // აქ ჩაამატებ ყვლეა ალბომს;
+    albumList += "</div>"
+}
+
+
